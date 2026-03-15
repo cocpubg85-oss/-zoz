d40f5bcd7a95c841b92f5a93f7fd69435fa03c5a
cocpubg85-oss <cocpubg85@gmail.com>
2026-03-15 07:21:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// string to numper
+const n ="10"
+const m = "20.2"
+console.log(parseInt(n) +parseFloat(m) );
+//arrayالمصفوفه 
+const langs = ["العربيه","الانجليزيه","اليونانيه"]
+console.log(langs)
+console.log(langs[2])
+console.log(langs[0])
+langs [0] ="pritsh"
+console.log(langs)
+//اضافه نص جديد فى اخر المصفوفه
+langs.push("العبريه")
+console.log(langs)
+//ازاله نص من اخر المصفوف
+langs.pop()
+console.log(langs)
+//objects
+const person ={
+    id = 10808,
+    firstName = "hazem",
+    adress = "cairo",
+    hobbies = ["foot ball", "panting"]
+
+}
+console.log(person)
